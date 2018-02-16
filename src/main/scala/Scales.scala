063368318f45d8c74cbd154332dff1cfadec10b9
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-16 13:37:18 +0000

    initial commit

---
+++ src/main/scala/Scales.scala
@@ -0,0 +1,17 @@
+class BalanceNotPossibleException extends Exception
+
+object Scales {
+
+  /*
+  * A function that will return a String of one or two numbers which
+  * represent the values needed to balance a pair of weights on a scale.
+  *
+  * input format: "[1,2], [4,5,6]"
+  *  - [1,2] represents weights to be balanced
+  *  - [4,5,6] represents the weights available to achieve this balance
+  * */
+
+  def balance(input: String): String = ???
+
+}
+
---
+++ src/test/scala/ScalesSpec.scala
@@ -0,0 +1,37 @@
+import org.scalatest.{MustMatchers, Wordspec}
+
+
+class ScalesSpec extends WordSpec with MustMatchers {
+
+  "balance" must {
+
+    "throw a BalanceNotPossibleException when given input '[3,4], []'" in {
+      intercept[BalanceNotPossibleException] {
+        ScaleBalancer.balance("[3,4],[]")
+      }
+    }
+
+    "throw a BalanceNotPossibleException when given input '[3,4], [10]'" in {
+      intercept[BalanceNotPossibleException] {
+        ScaleBalancer.balance("[3,4],[10]")
+      }
+    }
+
+    "return 1 when given the input '[3,4],[1]'" in {
+      ScaleBalancer.balance("[3,4],[1]") mustEqual "1"
+    }
+
+    "return '6,2' when given the input '[5, 9], [1, 2, 6, 7]'" in {
+      ScaleBalancer.balance("[5,9],[1,2,6,7]") mustEqual "6,2"
+    }
+
+    "throw a BalanceNotPossibleException when given the input '[4,7], [1,4]" in {
+      intercept[BalanceNotPossibleException] {
+        ScaleBalancer.balance("[4,7], [1,4]")
+      }
+    }
+
+  }
+
+}
+
